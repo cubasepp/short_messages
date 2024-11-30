bb1a72f99c4d9caf830c090766d923732b8ba9b0
Michael Vogl <michael.vogl@hey.com>
2024-11-30 23:29:17 +0100

    Add chunk spliting
    Split sentences that are longer as n characters. Append a suffix

--- spec/models/short_message_spec.rb
+++ spec/models/short_message_spec.rb
@@ -9,12 +9,40 @@ RSpec.describe ShortMessage, type: :model do
 
 
   describe "#message" do
-    context "split chunks" do
-      let(:message) { Faker::Lorem.sentence }
+    before do
+      ShortMessage.max_chunk_length = 20
+    end
 
-      it "splits 4 Words into 4 chunks" do
+    context "split chunks" do
+      let(:message) {  }
+
+      it "splits 4 Words into 3 chunks" do
+        subject.message = "Voluptas placeat ea est."
+        expect(subject.message_chunks).to(match_array([ "Voluptas- part 1", "placeat ea- part 2", "est.- part 3" ]))
+        expect(subject.valid?).to(be(true))
+      end
+
+      it "splits 5 Words into 5 chunks" do
+        subject.message = "LoremImpsumNovum NovumImpsumLorem LoremImpsumNovum NovumImpsumLorem LoremImpsumNovum "
+        expect(subject.message_chunks).to(match_array([
+          "LoremImpsumNovum- part 1",
+          "NovumImpsumLorem- part 2",
+          "LoremImpsumNovum- part 3",
+          "NovumImpsumLorem- part 4",
+          "LoremImpsumNovum- part 5"
+        ]))
+        expect(subject.valid?).to(be(false))
+      end
+    end
+
+    context "chunks to string" do
+      let(:message) { "Voluptas placeat ea est." }
+      it "converts a chunks split into a string" do
         subject.message = message
-        expect(subject.message_chunks).to(match_array(message.split))
+        subject.save!
+
+        expect(subject.message_chunks).to(match_array([ "Voluptas- part 1", "placeat ea- part 2", "est.- part 3" ]))
+        expect(subject.message).to(eql(message))
       end
     end
   end
